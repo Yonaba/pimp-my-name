8fbdf797f88a2017c1442b694a3e6933799e7db9
Yonaba <roland.yonaba@gmail.com>
2014-09-09 13:48:14 +0000

    Added index

---
+++ src/index.lua
@@ -0,0 +1,19 @@
+while true do
+	screen:clear()
+	pad = Controls.read()
+
+	if pad:cross() then
+		txtmsg = System.startOSK("Enter Name","Enter Name")
+		System.message(txtmsg,0)
+	end
+
+	file = io.open("name.txt" , "w")
+	file:write(txtmsg)
+	file:close()
+
+	dofile("pimpin_list.lua")
+	
+	screen.waitVblankStart()
+	screen.flip()
+end
+
